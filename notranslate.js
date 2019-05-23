8da0f090f93b98dba4b1035241b950cdda1c8e17
djeeno <djeeeno@gmail.com>
2019-05-23 23:48:04 +0900

    Update notranslate.js

--- notranslate.js
+++ notranslate.js
@@ -4,10 +4,11 @@ if (location.hostname === 'github.com' || location.hostname === 'gist.github.com
     Array.prototype.forEach.call(document.getElementsByClassName('content'), x => x.classList.add('notranslate'))
     Array.prototype.forEach.call(document.getElementsByClassName('file'), x => x.classList.add('notranslate'))
     Array.prototype.forEach.call(document.getElementsByClassName('final-path'), x => x.classList.add('notranslate'))
-    Array.prototype.forEach.call(document.getElementsByTagName('code'), x => x.classList.add('notranslate'))
-    Array.prototype.forEach.call(document.getElementsByTagName('pre'), x => x.classList.add('notranslate'))
 }
 
 if (location.hostname === 'blog.discordapp.com') {
     Array.prototype.forEach.call(document.getElementsByTagName('iframe'), x => x.classList.add('notranslate'))
 }
+
+Array.prototype.forEach.call(document.getElementsByTagName('code'), x => x.classList.add('notranslate'))
+Array.prototype.forEach.call(document.getElementsByTagName('pre'), x => x.classList.add('notranslate'))
